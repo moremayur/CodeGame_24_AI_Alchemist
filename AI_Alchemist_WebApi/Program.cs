d4c13df08ec52e541b2bd2b8fd59b81c49cf3f44
AP\Mayur.More <mayur.more@walters.kluwer.com>
2024-05-21 11:42:45 +0530

    Basic .net core webapi solution

---
+++ AI_Alchemist_WebApi/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"));
+});
+
+builder.Services.AddHealthChecks();
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHealthChecks("/health");
+});
+
+app.MapControllers();
+
+app.Run();
