1dea366c3c2f2fc44d96dc308729ec89203c26aa
AP\Mayur.More <mayur.more@walters.kluwer.com>
2024-05-22 12:36:05 +0530

    Adding more mock data

--- AI_Alchemist_WebApi/Controllers/WebhookController.cs
+++ AI_Alchemist_WebApi/Controllers/WebhookController.cs
@@ -22,32 +22,88 @@ namespace AI_Alchemist_WebApi.Controllers
                 request = jsonParser.Parse<WebhookRequest>(reader);
             }
 
-            var pas = request.QueryResult.Parameters;
-            var askingDocumentName = pas.Fields.ContainsKey("documentname") && pas.Fields["documentname"].ToString().Replace('\"', ' ').Trim().Length > 0;
-            var documentName = pas.Fields["documentname"].ToString().Replace('\"', ' ').Trim();
-
+            var intent = request.QueryResult.Intent;
+            var param = request.QueryResult.Parameters;
+            
             var response = new WebhookResponse();
+
+            //var isIntentDisplayName = pas.Fields.ContainsKey("displayName") && pas.Fields["displayName"].ToString().Replace('\"', ' ').Trim().Length > 0;
+            //var displayName = pas.Fields["displayName"].ToString().Replace('\"', ' ').Trim();
+
 
             StringBuilder sb = new StringBuilder();
 
-            if (askingDocumentName)
+            if (!string.IsNullOrEmpty(intent.DisplayName))
             {
-                sb.Append("The document name is: " + documentName + "; ");
+                var displayName = intent.DisplayName?.Replace('\"', ' ').Trim();
 
-                JObject jObject = JObject.Load(new JsonTextReader(System.IO.File.OpenText("documentInfoResponse.json")));
+                JObject mockDocInfoResponse = JObject.Load(new JsonTextReader(System.IO.File.OpenText("documentInfoResponse.json")));
+                JArray mockDocs = (JArray)mockDocInfoResponse["documents"];
 
-                JArray resources = (JArray)jObject["documents"];
+                JObject mockReleaseInfo = JObject.Load(new JsonTextReader(System.IO.File.OpenText("releaseFeaturesInfo.json")));
+                JArray mockReleases = (JArray)mockReleaseInfo["releases"];
 
-                JToken resObj = resources.Where(obj => obj["Name"].Value<string>() == documentName).SingleOrDefault();
 
-                if (resObj != null)
+                if (displayName.Equals("document.history", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    sb.Append("and status is " + resObj["Status"].ToString());
+                    var documentCount = param.Fields["documentcount"]?.ToString().Replace('\"', ' ').Trim();
+
+                    if (!string.IsNullOrEmpty(documentCount))
+                    {
+                        List<JToken> documents = mockDocs
+                            .OrderByDescending(x => x["DateCreated"].Value<DateTime>())
+                            .Select(x => x)
+                            .Take(Convert.ToInt16(documentCount))
+                            .ToList();
+
+                        int cnt = 1;
+                        foreach (JToken doc in documents)
+                        {
+                            sb.Append(cnt + ".\n" + "DocumentName: " + doc["Name"].ToString() + "\nStatus: " + doc["Status"].ToString() + "\n");
+                            cnt++;
+                        }
+                    }
                 }
-                else
+                else if (displayName.Equals("document.status", StringComparison.InvariantCultureIgnoreCase))
                 {
-                    sb.Clear();
-                    sb.Append("This " + documentName + " is not found!");
+                    var documentName = param.Fields["documentname"].ToString().Replace('\"', ' ').Trim();
+
+                    if (!string.IsNullOrEmpty(documentName))
+                    {
+                        JToken resObj = mockDocs.Where(obj => obj["Name"].Value<string>().ToLower() == documentName.ToLower()).SingleOrDefault();
+
+                        if (resObj != null)
+                        {
+                            sb.Append($"The status of { documentName} is {resObj["Status"].ToString()}.");
+                        }
+                        else
+                        {
+                            sb.Clear();
+                            sb.Append("The " + documentName + " is not found!");
+                        }
+                    }
+                }
+                else if (displayName.Equals("application.featues", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var versioinNo = param.Fields["version"].ToString().Replace('\"', ' ').Trim();
+
+                    if (!string.IsNullOrEmpty(versioinNo))
+                    {
+                        sb.Append($"The {versioinNo} release contains features, ");
+
+                        JToken resObj = mockReleases.Where(obj => obj["number"].Value<string>().ToLower() == versioinNo.ToLower()).SingleOrDefault();
+
+                        if (resObj != null)
+                        {
+                            var features = resObj["features"];
+                            sb.Append("\n" + string.Join(',', features));
+                        }
+                        else
+                        {
+                            sb.Clear();
+                            sb.Append($"Information about this version {versioinNo} is not available!");
+                        }
+                    }
                 }
             }
 
